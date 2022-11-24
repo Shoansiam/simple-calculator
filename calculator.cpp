9f34d9c02a9ee281c23128aa9867a1dfda954d7a
Shoan_siam <118611514+Shoansiam@users.noreply.github.com>
2022-11-24 11:05:50 +0600

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    int plus, del, mult, devid;
+    
+    cout<<"The first number:\n";
+    cin>>a;
+
+    cout<<"The sec number:\n";
+    cin>>b;
+    plus = a+b;
+    del = a-b;
+    mult = a*b;
+    devid = a/b;
+
+    
+    cout<<"the plus of two numbers:"<<plus<<endl;
+    cout<<"the del of two number is:"<<del<<endl;
+    cout<<"the mult of two number is:"<<mult<<endl;
+    cout<<"the devid of two number is:"<<devid<<endl;
+
+
+    return 0;
+}
